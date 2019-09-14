39f7d0e2c19c0a0002966a1863c98348e4968ab4
CindyLyu <xd753159@gmail.com>
2019-09-14 17:46:36 +0800

    add blog-SPA completed

---
+++ build/precache-manifest.26d09dd4378458f2a4ad36e42999e892.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d5cc0729f68672024fb2511486d24f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba285194f8f7a51d3631",
+    "url": "/static/css/main.ea0a49f7.chunk.css"
+  },
+  {
+    "revision": "01b7390ff6eadc015856",
+    "url": "/static/js/2.01e6883c.chunk.js"
+  },
+  {
+    "revision": "ba285194f8f7a51d3631",
+    "url": "/static/js/main.db25179d.chunk.js"
+  },
+  {
+    "revision": "cb4fd463e7184abd8f6a",
+    "url": "/static/js/runtime~main.d52357dd.js"
+  }
+]);
